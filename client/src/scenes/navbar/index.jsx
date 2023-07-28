4af2a91ac1f46da01d268d72619636330a28516b
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-07-29 03:00:48 +0530

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          DevNetwork
+          DevTown
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
